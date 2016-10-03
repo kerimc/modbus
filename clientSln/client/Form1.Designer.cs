6e8a4e9fd632cb764d5e2a2c4afacdd8a7787ace
kerimc <mirek.chomicz@gmail.com>
2016-10-03 23:47:56 +0200

    initial commit work on Form1

---
+++ clientSln/client/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstBaudrate = new System.Windows.Forms.ComboBox();
+            this.lstPorts = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.lstParity = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstDataBits = new System.Windows.Forms.ComboBox();
+            this.lstStopBits = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstStopBits);
+            this.groupBox1.Controls.Add(this.lstDataBits);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lstParity);
+            this.groupBox1.Controls.Add(this.lstBaudrate);
+            this.groupBox1.Controls.Add(this.lstPorts);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 192);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PC Port Info";
+            // 
+            // lstBaudrate
+            // 
+            this.lstBaudrate.FormattingEnabled = true;
+            this.lstBaudrate.Location = new System.Drawing.Point(90, 40);
+            this.lstBaudrate.Name = "lstBaudrate";
+            this.lstBaudrate.Size = new System.Drawing.Size(82, 21);
+            this.lstBaudrate.TabIndex = 3;
+            // 
+            // lstPorts
+            // 
+            this.lstPorts.FormattingEnabled = true;
+            this.lstPorts.Location = new System.Drawing.Point(90, 13);
+            this.lstPorts.Name = "lstPorts";
+            this.lstPorts.Size = new System.Drawing.Size(82, 21);
+            this.lstPorts.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baudrate:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port:";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(6, 229);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(178, 37);
+            this.btnScan.TabIndex = 3;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            // 
+            // lstParity
+            // 
+            this.lstParity.FormattingEnabled = true;
+            this.lstParity.Location = new System.Drawing.Point(90, 70);
+            this.lstParity.Name = "lstParity";
+            this.lstParity.Size = new System.Drawing.Size(82, 21);
+            this.lstParity.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Parity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Data bits";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Stop bits";
+            // 
+            // lstDataBits
+            // 
+            this.lstDataBits.FormattingEnabled = true;
+            this.lstDataBits.Location = new System.Drawing.Point(90, 101);
+            this.lstDataBits.Name = "lstDataBits";
+            this.lstDataBits.Size = new System.Drawing.Size(82, 21);
+            this.lstDataBits.TabIndex = 9;
+            // 
+            // lstStopBits
+            // 
+            this.lstStopBits.FormattingEnabled = true;
+            this.lstStopBits.Location = new System.Drawing.Point(90, 128);
+            this.lstStopBits.Name = "lstStopBits";
+            this.lstStopBits.Size = new System.Drawing.Size(82, 21);
+            this.lstStopBits.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 573);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox lstBaudrate;
+        private System.Windows.Forms.ComboBox lstPorts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ComboBox lstParity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox lstStopBits;
+        private System.Windows.Forms.ComboBox lstDataBits;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ clientSln/client/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        modbus mb = new modbus();
+        SerialPort sp = new SerialPort();
+        System.Timers.Timer timer = new System.Timers.Timer();
+        string dataType;
+        bool isPolling = false;
+        int pollCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadListboxes();
+        }
+
+        private void LoadListboxes()
+        {
+            //Three to load - ports, baudrates, datetype.  Also set default textbox values:
+            //1) Available Ports:
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                lstPorts.Items.Add(port);
+            }
+
+            lstPorts.SelectedIndex = 0;
+
+            //2) Baudrates:
+            string[] baudrates = { "230400", "115200", "57600", "38400", "19200", "9600" };
+
+            foreach (string baudrate in baudrates)
+            {
+                lstBaudrate.Items.Add(baudrate);
+            }
+
+            lstBaudrate.SelectedIndex = 1;
+
+            //3 Parity
+            Parity[] parities = {0, (Parity)1, (Parity)2, (Parity)3 };
+
+            foreach (Parity parityMode in parities)
+            {
+                lstParity.Items.Add(parityMode);
+            }
+            lstParity.SelectedIndex = 2;
+            //4 DataBits
+            int[] dataBits = { 7,8};
+
+            foreach (int dataBit in dataBits)
+            {
+                lstDataBits.Items.Add(dataBit);
+            }
+            lstDataBits.SelectedIndex = 1;
+           //4 Stop bits
+           StopBits[] stopBits = { 0, (StopBits)1, (StopBits)2, (StopBits)3 };
+
+            foreach (int stopBit in stopBits)
+            {
+                lstStopBits.Items.Add(stopBit);
+            }
+            lstStopBits.SelectedIndex = 1;
+            //3) Datatype:
+
+            /*
+            string[] dataTypes = { "Decimal", "Hexadecimal", "Float", "Reverse" };
+
+            foreach (string dataType in dataTypes)
+            {
+                lstDataType.Items.Add(dataType);
+            }
+
+            lstDataType.SelectedIndex = 0;
+
+            //Textbox defaults:
+            txtRegisterQty.Text = "20";
+            txtSampleRate.Text = "1000";
+            txtSlaveID.Text = "1";
+            txtStartAddr.Text = "0";
+            */
+        }
+
+        private void Scan1()
+        {
+            //Open COM port using provided settings:
+            if (mb.Open(lstPorts.SelectedItem.ToString(), 
+                Convert.ToInt32(lstBaudrate.SelectedItem.ToString()),
+                Convert.ToInt32(lstDataBits.SelectedItem.ToString()),
+                (Parity)Enum.Parse(typeof(Parity), lstParity.SelectedItem.ToString()),
+                (StopBits)Enum.Parse(typeof(StopBits), lstStopBits.SelectedItem.ToString())));
+
+            {
+                //Disable double starts:
+                btnScan.Enabled = false;
+               
+                //Set polling flag:
+                isPolling = true;
+
+               
+            }
+
+           //blStatus.Text = mb.modbusStatus;
+        }
+    }
+ }
+
